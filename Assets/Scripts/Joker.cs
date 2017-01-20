99b6be8726e4f99265f3265a23675d1d26a4a78c
Yene2017 <yenezhan@gmail.com>
2017-01-20 23:46:02 +0800

    joker&person

---
+++ Assets/Scripts/Joker.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Joker : MonoBehaviour 
+{
+	public static Vector2 center;
+	public static Vector2 sensitive;
+	public static List<Joker> jokers=new List<Joker>();
+	public static Rect clamp;
+
+	public static void Move()
+	{
+		var hori =Input.GetAxis( "Horizontal" );
+ 		var velt =Input.GetAxis( "Veltical" ); 
+		if( hori!=0 )
+		{
+			center.x+=hori*sensitive.x;
+		}
+		if( velt!= 0)
+		{
+			center.y+=velt*sensitive.y;
+		}
+		center.x=Mathf.Clamp( center.x, clamp.xMin, clamp.xMax );
+		center.y=Mathf.Clamp( center.y, clamp.yMin, clamp.yMax );
+	}
+
+	public static void High(Vector2 mouse, float radius)
+	{
+		foreach( var j in jokers )
+		{
+			if( Vector2.SqrMagnitude( mouse-center-j.location )<radius )
+			{
+				j.High();
+			}
+		}
+	}
+
+
+	public Vector2 location;
+	public Vector2 size=new Vector2( 10, 10 );
+	public float high=10;
+
+	public void High()
+	{
+	
+	}
+
+	public void OnMouseDown()
+	{
+		var rect=new Rect();
+		rect.position=center+location-size/2;
+		rect.size=size;
+		Person.High( rect, high );
+	}
+
+}
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Person : MonoBehaviour 
+{
+	public static bool inSpeech=false;
+	public static List<Person> people=new List<Person>();
+
+	public static void AddPerson(Vector2 location)
+	{
+		var go=new GameObject( "person" );
+		var p=go.AddComponent<Person>();
+		p.SetLocation( location );
+		people.Add( p );
+	}
+
+	public static void High(Rect area,float level)
+	{
+		foreach(var p in people)
+		{
+			if( area.Contains( p.location ) )
+			{
+				p.High( level );
+			}
+		}
+	}
+
+	[SerializeField]
+	Vector2 location;
+	[HideInInspector]
+	public float sentiment = 0;
+	[HideInInspector]
+	public float attraction=0;
+	public float maxAttraction=30;
+	public float calm = 1f;
+
+	public void SetLocation(Vector2 loc)
+	{
+		location=loc;
+	}
+
+	public void High(float level)
+	{
+		//影响观众
+		sentiment+=level;
+		attraction+=level;
+		// 吸引观众
+		if( attraction>=maxAttraction )
+		{
+			var loc=location
+				+Vector2.up*Random.Range( 1, 10 )
+				+Vector2.right*Random.Range( 1, 10 );
+			AddPerson(loc);
+			attraction-=maxAttraction;
+		}
+		
+	}
+
+	void Update()
+	{
+		if( !inSpeech )
+		{
+			attraction-=Time.deltaTime*calm;
+		}
+	}
+
+}
